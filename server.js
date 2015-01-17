24142cb48956b8b5df8e6e3abff8ef61d0ad144b
Hicham Naoufal <hicham24_1@hotmail.de>
2015-01-17 23:26:26 +0100

    Finally Mongo is runnig everything is fine

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(stylus.middleware(
 
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/mean');
+mongoose.connect('mongodb://127.0.0.1/mean');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind (console, 'connection error....'));
@@ -33,13 +33,22 @@ db.once('open', function callback(){
 	console.log('mean database opened');
 });
 
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+	mongoMessage = messageDoc.message;
+});
+
 app.get('/partials/:partialPath', function(req,res){
 
 	res.render('partials/'+req.params.partialPath);
 });
 
 app.get('*', function(req,res){
-	res.render('index');
+	res.render('index',{
+	mongoMessage: mongoMessage
+	});
 });
 
 var port = 38000;
